23331263a48900597c05591bc02a5f1d373009e7
SwampTG <adriansoareslps@gmail.com>
2025-03-24 08:09:32 -0300

    Configuring next app style, chakraui and localization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from 'next-intl/plugin';
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images:{
+    remotePatterns: [
+      { hostname: 'img-s-msn-com.akamaized.net' } 
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  // i18n: {
+  //   locales: ['en', 'pt-BR'],
+  //   defaultLocale: 'en',
+  // }
+};
+
+export default withNextIntl(nextConfig);
